3812610518664042707a69470c7b1025180499aa
Mary Brady <emceebrady@gmail.com>
2019-10-01 20:45:19 -0600

    commit test

--- server/models/food.js
+++ server/models/food.js
@@ -5,11 +5,13 @@ let schemaName = 'Food'
 
 let schema = new Schema({
   type: { type: String, required: true },
-  name: {type: String, required: true},
-  calories: {type: Number, required: true},
-  carbs: {type: Number, required: true},
-  fat: {type: Number, required: true},
-  protein: {type: Number, required: true}
+  name: { type: String, required: true },
+  calories: { type: Number, required: true },
+  carbs: { type: Number, required: true },
+  fat: { type: Number, required: true },
+  protein: { type: Number, required: true }
 })
+
+//THIS IS A TEST!!!
 
 module.exports = mongoose.model(schemaName, schema)

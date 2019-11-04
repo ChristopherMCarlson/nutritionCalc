aece46aa1204260287983d48f76f4b9923c6a977
Christopher Carlson <c.m.carlson13@gmail.com>
2019-11-03 19:36:49 -0700

    charts are rendering, time to make them dynamic!

--- client/src/components/Nutrition.vue
+++ client/src/components/Nutrition.vue
@@ -1,8 +1,13 @@
 <template>
     <v-container>
-      <div class="small">
-        <bar-chart :chartdata="chartData" :options="chartOptions"/>
-        <button @click="fillData()">Randomize</button>
+      <div>
+        <canvas id="food-dist-chart" height="[$vuetify.breakpoint.mdAndDown ? 1000 : 50]"></canvas>
+      </div>
+      <div>
+        <canvas id="total-cals-by-day-chart" height="[$vuetify.breakpoint.mdAndDown ? 1000 : 100]"></canvas>
+      </div>
+      <div>
+        <canvas id="weight-chart" height="[$vuetify.breakpoint.mdAndDown ? 1000 : 100]"></canvas>
       </div>
       <v-dialog v-model="addFoodModal" max-width="290">
         <v-card>
@@ -19,63 +24,120 @@
   </template>
   
   <script>
-  import { Bar } from 'vue-chartjs'
+  import Chart from 'chart.js';
 
   export default {
-    extends: Bar,
     data: () => ({
       name: '',
       calories: '',
       addFoodModal: false,
-      chartdata: {
-        labels: ['January', 'February'],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20]
+      weightChartData: {
+        type: 'line',
+        data: {
+          labels: ['9/1', '9/8', '9/15', '9/22', '9/29', '10/6', '10/13', '10/20'],
+          datasets: [{
+            label: 'Weight',
+            data: [264, 259, 254, 260, 255, 249, 243, 230],
+            fill: false
+          }]
+        },
+        options: {
+          responsive: true,
+          // maintainAspectRatio: false,
+          // scaleOverride: true,
+          lineTension: 1,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false,
+                padding: 10,
+              }
+            }]
+          },
+          title: {
+            display: true,
+            position: 'top',
+            fontsize: 80,
+            text: 'Weight'
+          },
+          legend: {
+            display: false
           }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false
-      },
-    }),
-    mounted () {
-      this.renderChart(this.chartdata, this.options)
-    },
-    methods: {
-      fillData () {
-        this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
-          datasets: [
-            {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }, {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }
-          ]
         }
       },
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      foodDistChartData: {
+        type: 'doughnut',
+        data: {
+          labels: [
+            'Breakfast',
+            'Lunch',
+            'Dinner'
+          ],
+          datasets: [{
+            data: [154, 320, 1300],
+            backgroundColor: ['blue', 'green', 'red']
+          }]
+        },
+        options: {
+          responsive: true,
+          title: {
+            display: true,
+            position: 'top',
+            fontsize: 80,
+            text: "Today's Calorie Distribution"
+          }
+        }
+      },
+      caloriesByDayChartData: {
+        type: 'bar',
+        data: {
+          labels: ['10/28', '10/29', '10/30', '10/31', '11/1', '11/2', '11/3'],
+          datasets: [{
+            data: [1800, 1526, 1200, 1365, 1589, 1845, 1365],
+            backgroundColor: ['blue', 'red', 'yellow', 'green', 'cyan', 'orange', 'purple']
+          }]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                padding: 10,
+              }
+            }]
+          },
+          title: {
+            display: true,
+            position: 'top',
+            fontsize: 80,
+            text: "Last Week's Calories"
+          }
+        }
       }
-    }
-    //methods: {
-    //  createFood() {
-    //    let food = {
-    //      name: this.name,
-    //      calories: this.calories,
+    }),
+    mounted () {
+      this.createChart('food-dist-chart', this.foodDistChartData)
+      this.createChart('total-cals-by-day-chart', this.caloriesByDayChartData)
+      this.createChart('weight-chart', this.weightChartData)
+    },
+    methods: {
+      createChart(chartId, chartData) {
+        const ctx = document.getElementById(chartId);
+        const myChart = new Chart(ctx, {
+          type: chartData.type,
+          data: chartData.data,
+          options: chartData.options,
+        });
+      },
+    },
+    //computed: {
+    //  weight() {
+    //    let wData = this.$store.getters.weightChartData
+    //    if (document.getElementById('weight-chart')) {
+    //      this.createChart('weight-chart', wData);
     //    }
-    //    console.log(food);
-    //    this.$store.dispatch('createFood', food)
-    //    this.name = ''
-    //    this.calories = ''
+    //    return "success"
     //  },
     //}
   };
